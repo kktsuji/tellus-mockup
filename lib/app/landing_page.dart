5355dc20863897191b5a9c60d59b4f00edb40210
Kouki Tsuji <cookieee.626+tech@gmail.com>
2020-07-08 12:41:58 +0900

    Add time tracker flutter app

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tellus_mockup/app/home/home_page.dart';
+import 'package:tellus_mockup/app/home/jobs/jobs_page.dart';
+import 'package:tellus_mockup/app/sign_in/sign_in_page.dart';
+import 'package:tellus_mockup/services/auth.dart';
+import 'package:tellus_mockup/services/database.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+
+    return StreamBuilder<User>(
+        stream: auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            User user = snapshot.data;
+            if (user == null) {
+              return SignInPage.create(context);
+            }
+            return Provider<User>.value(
+              value: user,
+              child: Provider<Database>(
+                create: (_) => FirestoreDatabase(uid: user.uid),
+                child: HomePage(),
+              ),
+            );
+          } else {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}

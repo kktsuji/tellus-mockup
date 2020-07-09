065fcfd86d1aad05a900c69a779bec3b0acdc9e9
Kouki Tsuji <cookieee.626+tech@gmail.com>
2020-07-09 23:13:19 +0900

    Add post

---
+++ lib/app/home/models/post.dart
@@ -0,0 +1,19 @@
+class Post {
+  Post({
+    this.id,
+    this.title,
+    this.speaker,
+    this.description,
+    this.url,
+    this.img,
+    this.fee,
+  });
+
+  final String id;
+  final String title;
+  final String speaker;
+  final String description;
+  final String url;
+  final int img;
+  final int fee;
+}
